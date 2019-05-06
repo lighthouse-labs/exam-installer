c68b32544a9fc92f9c3b1887f1e896b190f5260d
Nima Boscarino <nboscari@sfu.ca>
2019-05-06 14:49:05 -0700

    init! Full script + README

---
+++ README.md
@@ -0,0 +1,23 @@
+## Exam Installer for Lighthouse Labs
+
+Usage:
+
+Install this package globally, to be used as a command line tool:
+
+```
+npm i -g exam-installer
+```
+
+Then, from your root directory (e.g. `/vagrant` in your Vagrant) run:
+
+```
+exam-installer <GitHub Username> <Test ID>
+```
+
+For example, I might run:
+
+```
+exam-installer NimaBoscarino web-01
+```
+
+Make sure you use the GitHub username that you have registered on Compass. Your instructor will provide you with the proper test ID. This will take a little while to run!
---
+++ bin/index.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+var shell = require("shelljs");
+const fs = require('fs');
+const chalk = require('chalk');
+
+const username = process.argv[2]
+const testId = process.argv[3]
+
+shell.exec("git clone git@github.com:lighthouse-labs/assessment-exam-student.git")
+shell.cd("assessment-exam-student")
+shell.exec(`echo ${username} >> .student-id`)
+shell.exec("npm install --no-bin-links")
+shell.exec("npm run start-exam", testId)
+shell.exec("npm run question 0") // run first question for verification
+
+console.log(`
+... Install complete! You're ready to start.
+
+You can now open up the ${chalk.blue('assessment-exam-student')} folder in your text editor.
+
+Then, ${chalk.blue('cd')} into ${chalk.blue('assessment-exam-student')}.
+
+To answer the questions, edit the code in the ${chalk.blue('answers')} folder.
+
+To run the tests for a particular question, run:
+
+	${chalk.blue('npm run question <Question Number>')}
+
+For example:
+
+	${chalk.blue('npm run question 0')}
+
+There is no need to submit manually. Your answers are submitted automatically whenever you run the tests.
+
+${chalk.magenta('Note!')} It is okay for you to Google for syntax or function use, but do ${chalk.magenta('not')} Google for the answers to the particular questions.
+
+Have fun! 👩‍💻
+`)
